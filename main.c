e9f17ce223eb2a7538cf709b323dacba6aee0084
Jorge Catumba <jorgerev90@gmail.com>
2013-02-17 20:11:03 -0500

    yyout redirected.

--- main.c
+++ main.c
@@ -5,15 +5,16 @@
 #include <stdlib.h>
 
 extern FILE *yyin;
-extern int yyparse();
+extern FILE *yyout;
+extern int yyparse(void);
 
-int main (int argc, char *argv[]){
+int main (int argc, char const *argv[]){
     char command[256];
+    char output[256];
     FILE *fp;
     init_table();
 
-
-    while(1){
+    while (1) {
         printf ("[mfcalc]: ");
         memset (command, 0x00, sizeof(command));
         fgets (command, 100, stdin);
@@ -21,7 +22,14 @@ int main (int argc, char *argv[]){
         fprintf(fp, "%s", command);
         yyin=fp;
         yyparse();
+        fscanf(yyout, "%s", output);
+        printf("%s\n", output);
         fclose(fp);
     }
 
 }
+
+/*int main(int argc, char const *argv[]){
+    init_table ();
+    return yyparse ();
+}*/
